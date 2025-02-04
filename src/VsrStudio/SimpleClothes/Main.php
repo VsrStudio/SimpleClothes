4793e53e051263c97bbc32447de5f2776dde5447
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2025-02-04 08:32:44 +0700

    add particle

--- src/VsrStudio/SimpleClothes/Main.php
+++ src/VsrStudio/SimpleClothes/Main.php
@@ -24,6 +24,7 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\{PlayerChangeSkinEvent, PlayerJoinEvent};
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\permission\DefaultPermissions;
@@ -37,6 +38,33 @@ use VsrStudio\SimpleClothes\Form\SimpleForm;
 use VsrStudio\SimpleClothes\Form\ModalForm;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
+use pocketmine\math\Vector3;
+use pocketmine\level\Level;
+use pocketmine\scheduler\Task;
+use pocketmine\scheduler\TaskScheduler;
+use VsrStudio\SimpleClothes\task\Schelud;
+use VsrStudio\SimpleClothes\Commands;
+//Particles
+use pocketmine\world\particle\EnchantParticle;
+use pocketmine\world\particle\EnchantmentTableParticle;
+use pocketmine\world\particle\PortalParticle;
+use pocketmine\world\particle\FlameParticle;
+use pocketmine\world\particle\ExplodeParticle;
+use pocketmine\world\particle\EntityFlameParticle;
+use pocketmine\world\particle\WaterParticle;
+use pocketmine\world\particle\WaterDripParticle;
+use pocketmine\world\particle\LavaParticle;
+use pocketmine\world\particle\LavaDripParticle;
+use pocketmine\world\particle\HeartParticle;
+use pocketmine\world\particle\AngryVillagerParticle;
+use pocketmine\world\particle\HappyVillagerParticle;
+use pocketmine\world\particle\CriticalParticle;
+use pocketmine\world\particle\InkParticle;
+use pocketmine\world\particle\SporeParticle;
+use pocketmine\world\particle\SmokeParticle;
+use pocketmine\world\particle\SnowballPoofParticle;
+use pocketmine\world\particle\RedstoneParticle;
+use pocketmine\world\particle\FloatingTextParticle;
 
 class Main extends PluginBase implements Listener {
 	
@@ -50,18 +78,36 @@ class Main extends PluginBase implements Listener {
     private Config $pdata;
 
     /** @var int*/
+    public $config;
+    public $deco;
+    public $particle;
+    public $colors = array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
+    public $types = array("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18");
+    public $unregister = array("particle");
     public $json;
 	
 	public function onEnable(): void{
 		self::$instance = $this;
     	$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	$this->saveResource("config.yml");
+	@mkdir($this->getDataFolder()."players/");
 
+        $server = $this->getServer();
+        $server->getCommandMap()->register("particle", new Commands($this));
+         //Unregister
+         foreach($this->unregister as $disable){
+          $commandMap = $this->getServer()->getCommandMap();
+          $command = $commandMap->getCommand($disable);
+          $command->setLabel($disable."_disabled");
+          $command->unregister($commandMap);
+          }
+        
         $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
         $this->pdata = new Config($this->getDataFolder() . "data.yml", Config::YAML);
     	$this->checkSkin();
     	$this->checkRequirement();
-	$this->checkAvailableSkins();
+    	$this->checkAvailableSkins();
     	$this->getLogger()->info($this->json . " Geometry Skin Confirmed");
 	
 	if(is_array($this->cfg->get("standard_capes"))) {
@@ -77,6 +123,17 @@ class Main extends PluginBase implements Listener {
 	public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $this->skin[$player->getName()] = $player->getSkin();
+        
+        // var_dump($this->getDataFolder()."players/".$p->getPlayer()->getName());
+        if(!is_dir($this->getDataFolder()."players/".strtolower($player->getName()))){
+
+          @mkdir($this->getDataFolder()."players/".strtolower($player->getName()));
+          $playerData = fopen($this->getDataFolder()."players/".strtolower($player->getName())."/player.yaml", "w");
+          $data = "Particle: true\nType: NULL";
+          fwrite($playerData, $data);
+          fclose($playerData);
+          $this->deco = new Config($this->getDataFolder()."players/". strtolower($player->getName()) . "/player.yaml", Config::YAML);
+        }
         
         if(file_exists($this->getDataFolder() . $this->pdata->get($player->getName()) . ".png")) {
             $oldSkin = $player->getSkin();
@@ -150,6 +207,9 @@ class Main extends PluginBase implements Listener {
 				$this->HatsForm($player);
 				break;
 				case 4:
+				$this->PUI($player);
+				break;
+				case 5:
 				break;
 			}
 		});
@@ -159,10 +219,238 @@ class Main extends PluginBase implements Listener {
 		$form->addButton("§eGensinImpact Skin\n§rClick to open", 0,"textures/ui/dressing_room_skins");
 		$form->addButton("§eCapes\n§rClick to open", 0,"textures/ui/dressing_room_capes");
         $form->addButton("§eHats\n§rClick to open", 0,"textures/ui/dressing_room_customization");
+        $form->addButton("§eParticle\n§rClick to open", 0,"textures/ui/particles");
 		$form->addButton("Exit", 0,"textures/ui/realms_red_x");
 		$form->sendToPlayer($player);
 	}
+	
+	public function PUI($player){
+		$form = new SimpleForm(function($player, $data = null){
+			if($data === null){
+				return true;
+			}
+			if($data === 0){
+				$this->getServer()->dispatchCommand($player, "pa disable");
+				$player->sendMessage("§cParticle Disable.");
+				return true;
+			}
+			
+			$level = $player->getWorld();
+			$getx = round($player->getPosition()->getX());
+			$gety = round($player->getPosition()->getY());
+			$getz = round($player->getPosition()->getZ());
+			$vect = new Vector3($getx, $gety, $getz, $level);
+			
+			if($player->hasPermission($this->config->get((string)$data)["Particle"]["Permission"])){
+			    $this->getServer()->dispatchCommand($player, "pa set ".$this->config->get((string)$data)["Particle"]["Name"]);
+				$this->getServer()->dispatchCommand($player, "pa enable");
+				$player->sendMessage("§aParticle Enable.");
+			} else {
+				$player->sendMessage("§cYou do not have permission.");
+			}
+		});
+		$content = str_replace (["{player}"], [$player->getName()], $this->config->get("Content"));
+		$form->setTitle($this->config->get("Title"));
+		$form->setContent($content);
+		$form->addButton("§l§cDisable Particle\n§r§8Tap To Enter", 0, "textures/blocks/barrier");
+		for($i = 1;$i <= 100;$i++){
+			if($this->config->exists((string)$i)){
+				if($player->hasPermission($this->cfg->get((string)$i)["Particle"]["Permission"])){
+					$form->addButton($this->config->get((string)$i)["Button"]["Name"], 0, "textures/ui/check");
+				} else {
+					$form->addButton($this->config->get((string)$i)["Button"]["Name"], 0, "textures/ui/icon_lock");
+				}
+			}
+		}
+		$form->sendToPlayer($player);
+		return true;
+	}
+	
+	public function isOP($player, $type = NULL){
+      if($type === NULL){
+        return true;
+      }else{
+      
+      $level = $player->getWorld();
+      $getx = round($player->getPosition()->getX());
+      $gety = round($player->getPosition()->getY());
+      $getz = round($player->getPosition()->getZ());
+      $vect = new Vector3($getx, $gety, $getz, $level);
+       
+      switch($type){
+      case "0":
+        $player->getWorld()->addParticle($vect, new PortalParticle()); 
+        return true;
+      case "1":
+        $player->getWorld()->addParticle($vect, new FlameParticle()); 
+        return true;
+      case "2":
+        $player->getWorld()->addParticle($vect, new EntityFlameParticle()); 
+        return true;
+      case "3":
+        $player->getWorld()->addParticle($vect, new ExplodeParticle()); 
+        return true;
+      case "4":
+        $player->getWorld()->addParticle($vect, new WaterParticle()); 
+        return true;
+      case "5":
+        $player->getWorld()->addParticle($vect, new WaterDripParticle()); 
+        return true;
+      case "6":
+        $player->getWorld()->addParticle($vect, new LavaParticle()); 
+        return true;
+      case "7":
+        $player->getWorld()->addParticle($vect, new LavaDripParticle());
+        return true;
+      case "8":
+        $player->getWorld()->addParticle($vect, new HeartParticle()); 
+        return true;
+      case "9":
+        $player->getWorld()->addParticle($vect, new AngryVillagerParticle()); 
+        return true;
+      case "10":
+        $player->getWorld()->addParticle($vect, new HappyVillagerParticle()); 
+        return true;
+      case "11":
+        $player->getWorld()->addParticle($vect, new CriticalParticle());
+        return true;
+      case "12":
+        $player->getWorld()->addParticle($vect, new EnchantmentTableParticle()); 
+        return true;
+      case "13":
+        $player->getWorld()->addParticle($vect, new InkParticle()); 
+        return true;
+      case "14":
+        $player->getWorld()->addParticle($vect, new SporeParticle()); 
+        return true;
+      case "15":
+        $player->getWorld()->addParticle($vect, new SmokeParticle()); 
+        return true;
+      case "16":
+        $player->getWorld()->addParticle($vect, new SnowballPoofParticle()); 
+        return true;
+      case "17":
+        $player->getWorld()->addParticle($vect, new RedstoneParticle()); 
+        return true;
+      case "18":
+        $task = new Schelud($this, $player); 
+        $this->getScheduler()->scheduleDelayedTask($task,1*5); // Counted in ticks (1 second = 20 ticks)
+        return true;
+      }
 
+      }
+	}
+
+    public function onMove(PlayerMoveEvent $e){
+      $player = $e->getPlayer();
+      
+    
+      
+      if($this->getServer()->isOp($player->getName()) === true){
+        $this->deco = new Config($this->getDataFolder()."players/". strtolower($player->getName()) . "/player.yaml", Config::YAML);
+        $status = $this->deco->get("Particle");
+        $type = $this->deco->get("Type");
+        if($status === false){
+          return true;
+        }else{
+        $this->isOP($player, $type);
+          return true;
+        }
+      }
+      
+      $level = $player->getWorld();
+      $getx = round($player->getPosition()->getX());
+      $gety = round($player->getPosition()->getY());
+      $getz = round($player->getPosition()->getZ());
+      $vect = new Vector3($getx, $gety, $getz, $level);
+
+      $this->deco = new Config($this->getDataFolder()."players/". strtolower($player->getName()) . "/player.yaml", Config::YAML);
+      $status = $this->deco->get("Particle");
+      if($status === false){
+        return true;
+      }
+      
+
+      if($player->hasPermission("particle.portal")){
+        $player->getWorld()->addParticle($vect, new PortalParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.flame")){
+        $player->getWorld()->addParticle($vect, new FlameParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.entityflame")){
+        $player->getWorld()->addParticle($vect, new EntityFlameParticle($player)); 
+      
+    } 
+      if($player->hasPermission("particle.explode")){
+        $player->getWorld()->addParticle($vect, new ExplodeParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.water")){
+        $player->getWorld()->addParticle($vect, new WaterParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.waterdrip")){
+        $player->getWorld()->addParticle($vect, new WaterDripParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.lava")){
+        $player->getWorld()->addParticle($vect, new LavaParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.lavadrip")){
+        $player->getWorld()->addParticle($vect, new LavaDripParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.heart")){
+        $player->getWorld()->addParticle($vect, new HeartParticle(10, $player)); 
+      
+    }
+      if($player->hasPermission("particle.angryvillager")){
+        $player->getWorld()->addParticle($vect, new AngryVillagerParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.happyvillager")){
+        $player->getWorld()->addParticle($vect, new HappyVillagerParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.critical")){
+        $player->getWorld()->addParticle($vect, new CriticalParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.enchanttable")){
+        $player->getWorld()->addParticle($vect, new EnchantmentTableParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.ink")){
+        $player->getWorld()->addParticle($vect, new InkParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.spore")){
+        $player->getWorld()->addParticle($vect, new SporeParticle($player)); 
+      
+    }
+      if($player->hasPermission("particle.smoke")){
+        $player->getWorld()->addParticle($vect, new SmokeParticle($player)); 
+      
+    } 
+      if($player->hasPermission("particle.snowball")){
+        $player->getWorld()->addParticle($vect, new SnowballPoofParticle($player)); 
+      
+    } 
+      if($player->hasPermission("particle.redstone")){
+        $player->getWorld()->addParticle($vect, new RedstoneParticle($player)); 
+      
+    } 
+      
+      if($player->hasPermission("particle.floatingtxt")){
+        $task = new Schelud($this, $player); 
+        $this->getScheduler()->scheduleDelayedTask($task,1*5); // Counted in ticks (1 second = 20 ticks)
+  } 
+
+    }
+	
 	public function openCapesUI($player) {
         $form = new SimpleForm(function(Player $player, $data = null) {
             $result = $data;
